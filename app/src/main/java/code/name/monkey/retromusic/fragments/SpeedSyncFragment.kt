a886edc2a2b3ef75dc5f61d564a800ccbb6c1c5d
wh1te-star <sleeeping99@outlook.com>
2024-08-26 22:06:19 +0900

    Show the current maintaining time of the speed.

--- app/src/main/java/code/name/monkey/retromusic/fragments/SpeedSyncFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/SpeedSyncFragment.kt
@@ -1,8 +1,6 @@
 package code.name.monkey.retromusic.fragments
 
 import android.Manifest
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
@@ -21,7 +19,6 @@ import androidx.fragment.app.Fragment
 import code.name.monkey.retromusic.R
 import code.name.monkey.retromusic.databinding.FragmentSpeedSyncBinding
 import code.name.monkey.retromusic.util.logD
-import com.google.android.material.progressindicator.LinearProgressIndicator
 
 
 class SpeedSyncFragment : Fragment() {
@@ -31,6 +28,16 @@ class SpeedSyncFragment : Fragment() {
     private lateinit var locationListener: LocationListener
 
     private var progress = 0.0f
+    private var speed = 0.0f
+
+    enum class SpeedStage {
+        slow,
+        midium,
+        fast,
+    }
+    private var speedStage = SpeedStage.midium
+    private var stageStartTime = 0L
+    val waitTimeInMillis = 10000
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,13 +51,17 @@ class SpeedSyncFragment : Fragment() {
         locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
         startLocationUpdates()
 
+        stageStartTime = System.currentTimeMillis()
+
         return binding.root
     }
 
     private fun startLocationUpdates() {
         locationListener = object : LocationListener {
             override fun onLocationChanged(location: Location) {
-                onSpeedChanged(location.speed)
+                speed = location.speed
+                updateSpeedViews()
+                updateDurationViews()
             }
 
             override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
@@ -76,12 +87,11 @@ class SpeedSyncFragment : Fragment() {
         )
     }
 
-    private fun onSpeedChanged(speed: Float) {
+    private fun updateSpeedViews() {
         progress = (speed * 3.6f -10.0f) * 100.0f / 110f
 
         binding.speedProgressBar.progress = progress.toInt()
 
-        logD("speed: ${speed}, progress: ${progress}")
         val color = when {
             progress in 0.0..30.0 -> ContextCompat.getColor(requireContext(), code.name.monkey.appthemehelper.R.color.md_green_A700)
             progress in 30.0..50.0 -> ContextCompat.getColor(requireContext(), code.name.monkey.appthemehelper.R.color.md_blue_A400)
@@ -91,23 +101,65 @@ class SpeedSyncFragment : Fragment() {
         }
         binding.speedProgressBar.setIndicatorColor(color)
 
-        binding.speedTextView.text = getSpeedString((speed*3.6).toInt().toString())
-        binding.durationTextView.text = getSpeedString((speed*3.6).toString())
+        binding.speedTextView.text = getSpannedString((speed*3.6).toInt(), "km/h")
     }
 
-    fun getSpeedString(speed: String): SpannableString {
-        val text = "$speed km/h"
+    private fun updateDurationViews() {
+        val speedMaintainedDuration = System.currentTimeMillis() - stageStartTime
+        val speedKmH = speed * 3.6
+        if(speedStage == SpeedStage.slow){
+            if(speedKmH > 40.0) {
+                speedStage = SpeedStage.midium
+            }
+            stageStartTime = System.currentTimeMillis()
+        } else if (speedStage == SpeedStage.midium){
+            if(speedKmH in 10.0..30.0){
+                if(speedMaintainedDuration > waitTimeInMillis) {
+                    speedStage = SpeedStage.slow
+                }
+            } else if(speedKmH in 60.0..90.0) {
+                if (speedMaintainedDuration > waitTimeInMillis) {
+                    speedStage = SpeedStage.fast
+                }
+            }else if(speedKmH > 90.0){
+                speedStage = SpeedStage.fast
+                stageStartTime = System.currentTimeMillis()
+            } else {
+                stageStartTime = System.currentTimeMillis()
+            }
+        }else{
+            if(speedKmH < 60.0){
+                speedStage = SpeedStage.midium
+            }
+            stageStartTime = System.currentTimeMillis()
+        }
+
+        binding.durationTextView.text = getSpannedString((speedMaintainedDuration/1000).toInt(), "s")
+
+        binding.durationProgressBar.progress = (100.0 * speedMaintainedDuration / waitTimeInMillis).toInt()
+        val color = when (speedStage) {
+            SpeedStage.slow -> ContextCompat.getColor(requireContext(), code.name.monkey.appthemehelper.R.color.md_green_A700)
+            SpeedStage.midium -> ContextCompat.getColor(requireContext(), code.name.monkey.appthemehelper.R.color.md_blue_A400)
+            SpeedStage.fast -> ContextCompat.getColor(requireContext(), code.name.monkey.appthemehelper.R.color.md_red_A400)
+            else -> ContextCompat.getColor(requireContext(), R.color.dark_color)
+        }
+        binding.durationProgressBar.setIndicatorColor(color)
+    }
+
+    fun getSpannedString(number: Int, unitString: String): SpannableString {
+        val numberString = number.toString()
+        val text = "${numberString}${unitString}"
         val spannableString = SpannableString(text)
 
         spannableString.setSpan(
             RelativeSizeSpan(1.0f),
-            0, speed.length,
+            0, numberString.length,
             Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
         )
 
         spannableString.setSpan(
             RelativeSizeSpan(0.7f),
-            speed.length, text.length,
+            numberString.length, text.length,
             Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
         )
 
